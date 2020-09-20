f754b35b9fb88996d80cafc0ff7b2d092a616a49
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2020-09-19 18:12:48 -0600

    In scripts, create global variables, fetch functions, assign data to variables

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,55 @@
-import './css/styles.scss';
+
+// ************ IMPORTED FILES *************** //
+// import Traveler from './traveler';
+// import Destination from './destination';
+// import Trip from './trip';
+
+
+// ************ QUERY SELECTORS *************** //
+
+
+// ************ GLOBAL VARIABLES *************** //
+let allTravelers = [];
+let allDestinations = [];
+let allTrips = [];
+let traveler;
+let destination;
+let trip;
+
+// ************ EVENT LISTENERS *************** //
+
+
+// ************ FETCH REQUESTS/MAIN DATA *************** //
+function checkData() {
+  Promise.all([getTravelers(), getDestinations(), getTrips()])
+    .then(data => loadServerInfo(data))
+    .catch(error => console.log(error))
+}
+
+function getTravelers {
+  fetch("https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+}
+
+function getTrips {
+  fetch("https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/destinations/destinations", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+}
+
+function getDestinations {
+  fetch("https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/trips", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+}
+
+function loadServerInfo(allData) {
+  allTravelers = allData[0];
+  allDestinations = allData[1];
+  allTrips = allData[2]);
+  traveler = new Traveler(allTravelers[Math.floor(Math.random() * allUsersData.length)]);
+}
